78142d590ed25db21ed121f0ca8e163ca2c06b3b
Giannis Kalopisis <gianniskalopisis@gmail.com>
2020-02-05 15:48:23 +0200

    Add connection and transfer scripts. Also add server properties.

---
+++ scripts/transferScript.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/expect
+
+#This script transfer file or directory to the sdi1500059@195.134.67.93 at port 2244 with scp
+#Arguments: 1)file or directory
+#			2)destination directory
+
+set username sdi1500059
+set host 195.134.67.93
+set password F-Qrfarqh
+
+#get file or directory from command line
+set tranferData [lindex $argv 0];
+set path [lindex $argv 1];
+
+
+if {[file exists "$tranferData"]} { 
+	if {[file isdirectory "$tranferData"]} {
+		set type "directory"
+	} else {
+		set type "file"
+	}
+} else {
+	send_user "Error: $tranferData is not file or directory...\n"
+	exit 1
+}
+
+if {"$type" == "directory"} {
+	spawn scp -P 2244 -r $tranferData $username@$host:$path
+	expect {
+		"Are you sure you want to continue connecting*" {
+			send yes\r
+			exp_continue
+		}
+		"*password:*" {
+			send $password\r
+			interact
+		}
+	}
+} else {
+	spawn scp -P 2244 $tranferData $username@$host:$path
+	expect {
+		"Are you sure you want to continue connecting*" {
+			send yes\r
+			exp_continue
+		}
+		"*password:*" {
+			send $password\r
+			interact
+		}
+	}
+}
+
+
