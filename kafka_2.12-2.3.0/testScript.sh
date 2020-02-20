0ce59159ade9f30ca2f57633a2fcc96c30f06c67
gianniskalopisis <gianniskalopisis@gmail.com>
2020-02-20 11:05:48 +0200

    Get fullpath of producer performance script

--- kafka_2.12-2.3.0/testScript.sh
+++ kafka_2.12-2.3.0/testScript.sh
@@ -1,1 +1,48 @@
-../scripts/testScript.sh
+#!/bin/bash
+
+
+#
+#count and test arguments
+#
+if [ "$#" -ne 2 ]; then
+    echo "No arguments provided. Give topic and a producer-x.properties file for arguments."
+    exit 2
+fi
+
+if [[ -f "$2" ]]; then
+	TOPIC=$1
+	PROPERTIES=$2
+else
+ 	echo "$2 is not file"
+ 	exit 1
+fi 
+
+
+#
+#get script directory
+#
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+
+PATH="${DIR}/bin/kafka-producer-perf-test.sh"
+
+
+
+#
+#execute script :( 
+#
+for i in 10 100 1000 10000 100000; do
+	echo ""
+ 	echo "Number of Records: $((1000*1024*1024/$i)), record size: $i"
+ 	#./bin/kafka-run-class.sh org.apache.kafka.tools.ProducerPerformance 
+	SCRIPT="${PATH} --topic ${TOPIC} --num-records $((1000*1024*1024/${i})) --record-size ${i} --throughput -1 --producer.config ${PROPERTIES}"
+ 	"$SCRIPT"
+ 	echo "----------------------------------------------------------------------------------------------------"
+done;
+
+
