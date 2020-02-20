566ca503d20dcdb2a2a6c7a160b6a7187cff6574
gianniskalopisis <gianniskalopisis@gmail.com>
2020-02-20 00:08:46 +0000

    Add testScript and soft link to it at kafka folder

--- scripts/testScript.sh
+++ scripts/testScript.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 
-if [ "$#" -ne 3 ]; then
-    echo "No arguments provided. Give path, topic and a server.properties file for arguments."
+if [ "$#" -ne 2 ]; then
+    echo "No arguments provided. Give topic and a producer-x.properties file for arguments."
     exit 2
 fi
 
-if [[ -f "$3" ]]; then
-	PATH=$1
-	TOPIC=$2
-	PROPERTIES=$3
+if [[ -f "$2" ]]; then
+	TOPIC=$1
+	PROPERTIES=$2
 else
- 	echo "$3 is not file"
+ 	echo "$2 is not file"
  	exit 1
 fi 
 
-PATH="${PATH}bin/kafka-producer-perf-test.sh"
+PATH="./bin/kafka-producer-perf-test.sh"
 
 for i in 10 100 1000 10000 100000; do
 	echo ""
  	echo "Number of Records: $((1000*1024*1024/$i)), record size: $i"
- 	COMMAND="${PATH} --topic ${TOPIC} --num-records $((1000*1024*1024/${i})) --record-size ${i} --throughput -1 --producer.config ${PROPERTIES}"
- 	#$PATH --topic $TOPIC --num-records $((1000*1024*1024/$i)) --record-size $i --throughput -1 --producer.config $PROPERTIES
- 	echo $COMMAND
- 	eval "$COMMAND"
+ 	#./bin/kafka-run-class.sh org.apache.kafka.tools.ProducerPerformance 
+	SCRIPT="${PATH} --topic ${TOPIC} --num-records $((1000*1024*1024/${i})) --record-size ${i} --throughput -1 --producer.config ${PROPERTIES}"
+ 	#echo $SCRIPT
+ 	"$SCRIPT"
  	echo "----------------------------------------------------------------------------------------------------"
 done;
