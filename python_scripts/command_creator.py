0c68ce2e49f68b8f384091bc105fc79c79ff848d
gianniskalopisis <gianniskalopisis@gmail.com>
2020-04-10 17:20:58 +0300

    Add buffer.memory and make program more abstract

--- python_scripts/command_creator.py
+++ python_scripts/command_creator.py
@@ -1,40 +1,22 @@
-
 import sys
 
-def batchSize():
 
-    total_bytes = 500000000
-
-    replication = [1, 2, 5]
-    partition = [1, 2, 5]
-    message = [100, 500, 1000]
-
-    batch = [16384, 50000, 100000, 200000, 500000]
-
-    counter = 1
-    for rf in replication:
-        print("\n\n##########################")
-        print("# Open brokers from: {}-{} #".format(replication[0], rf))
-        print("##########################\n")
-        for part in partition:
-            if counter == (len(replication)*len(partition)*len(message)*len(batch)*replication.index(rf) + 1):
-                partitionCommands(partition[partition.index(part) - 1], replication[replication.index(rf) - 1], rf, part, True)
-            else:
-                partitionCommands(partition[partition.index(part) - 1], replication[replication.index(rf) - 1], rf, part, False)
-            for mes in message:
-                for b in batch:
-                    print("python3 ../python_scripts/change_parameter.py batch.size={} " 
-                    "../test_properties_files/producer_properties/producer-{}.properties ; ".format(b, rf))
-                    for i in range(1,4):
-                        print("./bin/kafka-producer-perf-test.sh "
-                            "--topic test-part-{}-rep-{} "
-                            "--num-records {} --record-size {} --throughput -1 "
-                            "--producer.config ../test_properties_files/producer_properties/producer-{}.properties "
-                            "> ../python_scripts/metrics_folder/{:04d}_{}_{}_{}_{} ; "\
-                            .format(part, rf, int(total_bytes/mes), mes, rf, counter, rf, part, mes, b))
-                        counter += 1
+total_bytes = 500000000
+replication = [1, 2, 5]
+partition = [1, 2, 5]
+message = [100, 500, 1000]
 
 
+parameterDict = {
+    "batch.size": [16384, 50000, 100000, 200000, 500000],
+    "buffer.memory": [10000000, 33554432, 100000000, 200000000, 500000000]
+}
+
+
+def printBrokers(from_broker, to_broker):
+    print("\n\n##########################")
+    print("# Open brokers from: {}-{} #".format(from_broker, to_broker))
+    print("##########################\n")
 
 
 def partitionCommands(previous_part, previous_rf, rf, part, first):
@@ -47,6 +29,42 @@ def partitionCommands(previous_part, previous_rf, rf, part, first):
             "--topic test-part-{}-rep-{} ; ".format(previous_part, rf))
     print("./bin/kafka-topics.sh --create --zookeeper 195.134.67.93:2181 " 
         "--topic test-part-{}-rep-{} --partitions {} --replication-factor {} ; ".format(part, rf, part, rf))
+
+
+def parameterChange(parameter, size, config_type, rf):
+    print("python3 ../python_scripts/change_parameter.py {}={} " 
+        "../test_properties_files/{}_properties/{}-{}.properties ; ".format(parameter, size, config_type, config_type,rf))
+
+
+def kafkaCommand(part, rf, record_num, mes_size, counter, measurement_size):
+    print("./bin/kafka-producer-perf-test.sh "
+        "--topic test-part-{}-rep-{} "
+        "--num-records {} --record-size {} --throughput -1 "
+        "--producer.config ../test_properties_files/producer_properties/producer-{}.properties "
+        "> ../python_scripts/metrics_folder/{:04d}_{}_{}_{}_{} ; "\
+        .format(part, rf, record_num, mes_size, rf, counter, rf, part, mes_size, measurement_size))
+
+
+#measurement_type = "batch.size"
+#parameter_type = "producer/server"
+def printCommands(parameter_list, measurement_type , parameter_type):
+
+    parameter = parameter_list
+
+    counter = 1
+    for rf in replication:
+        printBrokers(replication[0], rf)
+        for part in partition:
+            if counter == (len(replication)*len(partition)*len(message)*len(parameter)*replication.index(rf) + 1):
+                partitionCommands(partition[partition.index(part) - 1], replication[replication.index(rf) - 1], rf, part, True)
+            else:
+                partitionCommands(partition[partition.index(part) - 1], replication[replication.index(rf) - 1], rf, part, False)
+            for mes in message:
+                for par in parameter:
+                    parameterChange(measurement_type, par, parameter_type, rf)
+                    for i in range(1,4):
+                        kafkaCommand(part, rf, int(total_bytes/mes), mes, counter, par)
+                        counter += 1
 
 
 def bufferMemory():
@@ -72,9 +90,9 @@ if __name__ == '__main__':
     test_parameter = sys.argv[1]
 
     if test_parameter == "batch.size":
-        batchSize()
+        printCommands(parameterDict["batch.size"], "batch.size", "producer")
     elif test_parameter == "buffer.memory":
-        bufferMemory()
+        printCommands(parameterDict["buffer.memory"], "buffer.memory", "producer")
     else:
         wrongArgumentsFunc()
     
