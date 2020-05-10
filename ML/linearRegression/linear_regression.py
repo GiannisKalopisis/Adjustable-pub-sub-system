81739cdab76f68e235054b85e98553adcfdd5cd1
gianniskalopisis <gianniskalopisis@gmail.com>
2020-05-10 19:13:05 +0300

    Small changes to cross validation

--- ML/linearRegression/linear_regression.py
+++ ML/linearRegression/linear_regression.py
@@ -44,6 +44,7 @@ if __name__ == '__main__':
         kf = KFold(n_splits=percentage, random_state=None, shuffle=True)
 
         # first way with ready cross_validation
+        print("Cross Validation (sklearn):")
         for score_param in scoring_dict:
             score = cross_val_score(regressor, X, y, scoring=scoring_dict[score_param], cv=kf)
             print("%s:" % (score_param))
@@ -51,20 +52,21 @@ if __name__ == '__main__':
             print("Accuracy: %0.2f (+/- %0.2f)\n" % (score.mean(), score.std() * 2))
 
         # second way with my cross_validation
-        # R2, EV, MAE, MSE, MDE = [], [], [], [], []
-        # for train_index, test_index in kf.split(X.values):
-        #     X_train, X_test = X.values[train_index], X.values[test_index]
-        #     y_train, y_test = y.values[train_index], y.values[test_index]
-        #     regressor.fit(X_train, y_train) # test also fit_transform
-        #     R2.append(getR2Metrics(y_test, regressor.predict(X_test)))
-        #     EV.append(getExplainedVarianceMetrics(y_test, regressor.predict(X_test)))
-        #     MAE.append(getMeanAbsoluteErrorMetrics(y_test, regressor.predict(X_test)))
-        #     MSE.append(getMeanSquaredErrorMetrics(y_test, regressor.predict(X_test)))
-        #     MDE.append(getMedianAbsoluteErrorMetrics(y_test, regressor.predict(X_test)))
-        # for score_list in [R2, EV, MAE, MSE, MDE]:
-        #     print("%s:" % (scoring_dict_names[namestr(score_list, globals())[0]]))
-        #     # print(score)
-        #     print("Accuracy: %0.2f (+/- %0.2f)\n" % (np.mean(score_list), np.std(score_list) * 2))
+        print("Cross Validation (my code):")
+        R2, EV, MAE, MSE, MDE = [], [], [], [], []
+        for train_index, test_index in kf.split(X.values):
+            X_train, X_test = X.values[train_index], X.values[test_index]
+            y_train, y_test = y.values[train_index], y.values[test_index]
+            regressor.fit(X_train, y_train) # test also fit_transform
+            R2.append(getR2Metrics(y_test, regressor.predict(X_test)))
+            EV.append(getExplainedVarianceMetrics(y_test, regressor.predict(X_test)))
+            MAE.append(getMeanAbsoluteErrorMetrics(y_test, regressor.predict(X_test)))
+            MSE.append(getMeanSquaredErrorMetrics(y_test, regressor.predict(X_test)))
+            MDE.append(getMedianAbsoluteErrorMetrics(y_test, regressor.predict(X_test)))
+        for score_list in [R2, EV, MAE, MSE, MDE]:
+            print("%s:" % (scoring_dict_names[namestr(score_list, globals())[0]]))
+            # print(score)
+            print("Accuracy: %0.2f (+/- %0.2f)\n" % (np.mean(score_list), np.std(score_list) * 2))
 
     # classic linear regression
     else:
@@ -75,6 +77,7 @@ if __name__ == '__main__':
 
         y_pred = regressor.predict(X_test)
         # actual_pred_values = get_pred_values_dataframe(Y_test, Y_pred)    # print the actual values
+        print("No cross validation :")
         print(get_coefficient_dataframe(regressor, data.columns.tolist(), target))
         print()
 
