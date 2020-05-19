261db19f7ef69c1cc8e0b104d7375b67e28789a2
gianniskalopisis <gianniskalopisis@gmail.com>
2020-05-19 16:42:10 +0300

    CART code for GridSearchCV and cross validation

--- ML/linear_models/CART/cart.py
+++ ML/linear_models/CART/cart.py
@@ -1,6 +1,7 @@
-from sklearn.tree import DecisionTreeRegressor
+from sklearn import tree
 from sklearn.model_selection import train_test_split, KFold, GridSearchCV, cross_val_score
-from sklearn.metrics import mean_squared_error
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import mean_squared_error, make_scorer
 import time
 import pandas as pd
 import numpy as np
@@ -38,40 +39,64 @@ if __name__ == '__main__':
     data = readCSVpd(input_file)
     X, y = getInputTargetDataPd(data, target)
 
-    # parameters dictionary
-    param_dict = {'max_depth': np.arange(3,31),
-                  'min_samples_leaf': np.arange(2,11)}
+    # GridSearch preparation
+    depths = np.arange(3,31)
+    num_leafs = np.arange(2,31)
+    decisionTree = tree.DecisionTreeRegressor()
+    pipe = Pipeline(steps=[('decisionTree', decisionTree)])
+    parameters = dict(decisionTree__max_depth=depths,
+                      decisionTree__min_samples_leaf=num_leafs)
 
     # getting regressor with optimal parameters from grid search
     print("Starting gridSearch for tuning Decision Tree parameters.")
     start = time.clock()
-    decisionTreeRegressor = GridSearchCV(DecisionTreeRegressor(), param_dict)
+    DTR_gs = GridSearchCV(pipe, parameters)
     end = time.clock()
     print("Elapsed time for GridSearchCV: {}\n".format(end-start))
 
-    if CROSS_VAL:
+    # take best estimator
+    X_train, X_test, y_train, y_test = train_test_split(X.values, y.values, test_size=(percentage/100), random_state=None, shuffle=True)
+    DTR_gs.fit(X_train, y_train)
+    print('Best max depth:', DTR_gs.best_estimator_.get_params()['decisionTree__max_depth'])
+    print('Best min samples leaf:', DTR_gs.best_estimator_.get_params()['decisionTree__min_samples_leaf'])
+    print(DTR_gs.best_estimator_.get_params()['decisionTree'])
+    # my_DT_model = DTR_gs.best_estimator_
 
-        kf = KFold(n_splits=percentage, random_state=None, shuffle=True)
-
-        # cross_validation
-        print("Cross Validation (sklearn):")
-        for score_param in scoring_dict:
-            start = time.clock()
-            score = cross_val_score(decisionTreeRegressor, X.values, y.values, scoring=scoring_dict[score_param], cv=kf)
-            print("%s:" % (score_param))
-            # print(score)
-            print("Accuracy: %0.2f (+/- %0.2f)" % (score.mean(), score.std() * 2))
-            print("Elapsed time: {}\n".format(time.clock()-start))
-
-    else:
-
-        X_train, X_test, y_train, y_test = train_test_split(X.values, y.values, test_size=(percentage/100), random_state=None, shuffle=True)
+    # train model
+    # my_DT_model.fit(X_train, y_train)
+    # y_pred = my_DT_model.predict(X_test)
+    # print_metrics(y_test, y_pred)
+    kf = KFold(n_splits=percentage, random_state=None, shuffle=True)
+    cv_results = cross_val_score(DTR_gs, X, y, cv=kf, n_jobs=-1)
+    print(cv_results)
+    print(cv_results.mean())
+    print(cv_results.std())
 
 
-        start = time.clock()
-        decisionTreeRegressor.fit(X_train, y_train)
-        y_pred = decisionTreeRegressor.predict(X_test)
-        print("No cross validation:")
-        print_metrics(y_test, y_pred)
-        print("Elapsed time: {}".format(time.clock()-start))
+
+    # if CROSS_VAL:
+
+    #     kf = KFold(n_splits=percentage, random_state=None, shuffle=True)
+
+    #     # cross_validation
+    #     print("Cross Validation (sklearn):")
+    #     for score_param in scoring_dict:
+    #         start = time.clock()
+    #         score = cross_val_score(decisionTreeRegressor, X.values, y.values, scoring=scoring_dict[score_param], cv=kf)
+    #         print("%s:" % (score_param))
+    #         # print(score)
+    #         print("Accuracy: %0.2f (+/- %0.2f)" % (score.mean(), score.std() * 2))
+    #         print("Elapsed time: {}\n".format(time.clock()-start))
+
+    # else:
+
+    #     X_train, X_test, y_train, y_test = train_test_split(X.values, y.values, test_size=(percentage/100), random_state=None, shuffle=True)
+
+
+    #     start = time.clock()
+    #     decisionTreeRegressor.fit(X_train, y_train)
+    #     y_pred = decisionTreeRegressor.predict(X_test)
+    #     print("No cross validation:")
+    #     print_metrics(y_test, y_pred)
+    #     print("Elapsed time: {}".format(time.clock()-start))
 
